9a67606bbb15d9a4183ac73da74bb8b0b9ccbb95
206 <206@academicos.cesde.edu.co>
2023-02-09 08:53:17 -0500

    actividad clientes y metodos en el main

--- app/src/main/java/com/example/concesionario/MainActivity.java
+++ app/src/main/java/com/example/concesionario/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.concesionario;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Clientes(View view) {
+        Intent intclientes = new Intent(this, ClientesActivity2.class);
+        startActivity(intclientes);
+    }
+
+    public void Vehiculos(View view) {
+        Intent intvehiculos = new Intent(this, VehiculoActivity2.class);
+        startActivity(intvehiculos);
+    }
+
+    public void Ventas(View view) {
+        Intent intventas = new Intent(this, VentaActivity2.class);
+        startActivity(intventas);
     }
 }
